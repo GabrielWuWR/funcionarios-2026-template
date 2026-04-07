511675336c530232d9ef52309cded22de96616cf
GABRIEL JOSÉ SANTOS DA SILVA <25132745@CFP127ALUNO.LOCAL>
2026-04-07 14:01:49 -0300

    Js para a criação dos cards

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-'use strict'
+'use strict';
 
-import funcionarios from "./funcionarios.json" with { type: "json" }
+import funcionarios from "./funcionarios.json" with { type: "json" };
 
+function criarCard(funcionario) {
+    let card = document.createElement('div');
+    card.className = 'card';
+
+    let foto = document.createElement('img');
+    foto.src = `./img/${funcionario.imagem}`;
+    foto.alt = `Foto de ${funcionario.nome}`;
+
+    let nome = document.createElement('h3');
+    nome.textContent = funcionario.nome;
+
+    let cargo = document.createElement('span');
+    cargo.textContent = funcionario.cargo;
+
+    card.append(foto, nome, cargo);
+
+    return card;
+};
+
+const cards = funcionarios.map(criarCard);
+
+document.getElementById('container').replaceChildren(...cards);
